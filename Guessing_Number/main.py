8572227a8a9c442578dadfd353bf937c13923911
Vadepeer04 <102405641+Vadepeer@users.noreply.github.com>
2023-11-03 18:43:36 +0530

    Update main.py

--- Guessing_Number/main.py
+++ Guessing_Number/main.py
@@ -6,7 +6,7 @@ HARD_LEVEL_TURNS = 5
 
 #Function to check user's guess against actual answer.
 def check_answer(guess, answer, turns):
-  """checks answer against guess. Returns the number of turns remaining."""
+  
   if guess > answer:
     print("Too high.")
     return turns - 1
@@ -33,7 +33,7 @@ def game():
   #easyprint(f"Pssst, the correct answer is {answer}") 
 
   turns = set_difficulty()
-  #Repeat the guessing functionality if they get it wrong.
+  
   guess = 0
   while guess != answer:
     print(f"You have {turns} attempts remaining to guess the number.")
@@ -41,7 +41,6 @@ def game():
     #Let the user guess a number.
     guess = int(input("Make a guess: "))
 
-    #Track the number of turns and reduce by 1 if they get it wrong.
     turns = check_answer(guess, answer, turns)
     if turns == 0:
       print("You've run out of guesses, you lose.")
